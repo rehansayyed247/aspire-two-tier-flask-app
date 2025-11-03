9c15a0b84343adbc371e577db0b36c510da4605a
rehansayyed247 <rehansayyed247@gmail.com>
2025-11-03 15:17:16 +0530

    Fix unit test code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from unittest.mock import MagicMock, patch
 from app import app, mysql
